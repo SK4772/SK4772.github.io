8032d9ba8003a07005f6d0c4527154acd9f729e4
yourname <your_email@xxx.com>
2024-12-03 09:20:43 +0800

    Site updated: 2024-12-03 09:20:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2017/05/26/论文写作注意事项—Csk/","2024/12/01/迁移学习综述/"];function toRandomPost(){
+var posts=["2024/12/01/迁移学习综述/","2017/05/26/论文写作注意事项—Csk/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
