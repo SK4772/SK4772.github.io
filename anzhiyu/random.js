b2aff740fae3cf8a4ba0c4c51009ad18d3180713
yourname <your_email@xxx.com>
2024-12-16 17:38:39 +0800

    Site updated: 2024-12-16 17:38:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/06/2010-Domain-Adaptation-via-Transfer-Component-Analysis（TCA）/","2024/09/01/论文写作规范—Csk/","2024/09/01/科研常用网站及软件—Csk/","2024/09/01/迁移学习综述/"];function toRandomPost(){
+var posts=["2024/12/06/2010-Domain-Adaptation-via-Transfer-Component-Analysis（TCA）/","2024/09/01/科研常用网站及软件—Csk/","2024/09/01/论文写作规范—Csk/","2024/09/01/迁移学习综述/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
