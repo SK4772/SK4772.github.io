a75a25ec1dcb8aef1c3c9a0a66c43cd78022fc13
yourname <your_email@xxx.com>
2024-12-16 15:51:31 +0800

    Site updated: 2024-12-16 15:51:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2017/05/26/论文写作注意事项—Csk/","2024/12/01/迁移学习综述/"];function toRandomPost(){
+var posts=["2024/12/06/2010-Domain-Adaptation-via-Transfer-Component-Analysis（TCA）/","2017/05/26/论文写作注意事项—Csk/","2024/12/01/迁移学习综述/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
